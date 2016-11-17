c6ae5539e9fa4d5cb377697e6082888acd63a816
Ryan <ryanimpey@live.co.uk>
2016-11-17 00:27:03 +0000

    updates - week 7

--- web-authoring/assignment/js/main.js
+++ web-authoring/assignment/js/main.js
@@ -1,1 +1,12 @@
-main.js
+$('.circular-shop').click(function(){
+  $('html, body').animate({
+        scrollTop: $(".section-products-title").offset().top
+    }, 300);
+});
+
+var bgImageList = [
+  'https://img-new.cgtrader.com/items/36548/dc4c32cd9a/bmw-6-series-white-wireframe-car-3d-model-obj.jpg',
+  'https://img-new.cgtrader.com/items/36548/dc4c32cd9a/bmw-6-series-white-wireframe-car-3d-model-obj.jpg',
+  'https://img-new.cgtrader.com/items/36548/dc4c32cd9a/bmw-6-series-white-wireframe-car-3d-model-obj.jpg',
+  'https://img-new.cgtrader.com/items/36548/dc4c32cd9a/bmw-6-series-white-wireframe-car-3d-model-obj.jpg'
+]
